d448c40434e5a0ec5058e9eb67dfb01624c7d837
Dennis Bagot <dennisbagot1998@gmail.com>
2021-04-12 16:16:20 -0400

    add dropzone code

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,26 +1,57 @@
 import { Icon } from 'antd';
 import React from 'react'
 import Dropzone from 'react-dropzone';
+import Axios from 'axios'
+import { response } from 'express';
 
 function FileUpload() {
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: {'content-type': 'multipart/form-data'}
+    }
+    formData.append("file", files[0])
+
+    //Save the Image we chose inside the Node server
+    Axios.post('/api/product/uploadImage', formData, config)
+    .then(response => {
+      if(response.data.success) {
+
+      } else {
+        alert('Failed to save the image in server')
+      }
+    })
+    
+  }
+
   return (
     <div>
       <div style={{ display:'flex', justifyContent:'space-between' }}>
         <Dropzone
-          onDrop
+          onDrop={onDrop}
           multiple
           maxSize
         >
           {({getRootProps, getInputProps}) => (
-            <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex', alignItems:'center'}}
+            <div style={{ 
+              width:'300px', height:'240px', border:'1px solid lightgray',
+              display:'flex', alignItems:'center', justifyContent:'center'
+            }}
               {...getRootProps()}
             >
               <input {...getInputProps()}/>
               <Icon type="plus" style={{ fontSize: '3rem'}} />
             </div>
           )}
-
         </Dropzone>
+        <div style={{ 
+          display:'flex', width:'350px', height:'240px', overflowX:'scroll'}}
+        >
+          <div onClick>
+            <img />
+          </div>
+        </div>
       </div>
     </div>
   )
