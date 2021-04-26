7f23b5083861983233498e39e9a9ff6e61aa6aad
Dennis Bagot <dennisbagot1998@gmail.com>
2021-04-26 15:36:55 -0400

    add landing

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import Axios from 'axios';
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
+
+    useEffect(() => {
+    Axios.post('/api/porduct/getProducts')
+    .then(response => {
+        if(response.data.success){
+
+        } else {
+            alert('Failed to fetch products')
+        }
+    })
+    }, [])
+    
     return (
         <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+            
         </>
     )
 }
