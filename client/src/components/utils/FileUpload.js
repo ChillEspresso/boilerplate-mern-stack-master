1094b2d4a0ca423daf5bfee04aa7f5274961f54f
Dennis Bagot <dennisbagot1998@gmail.com>
2021-04-26 14:48:44 -0400

    Fixed Prop Error

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,18 +1,69 @@
-import React from 'react'
-import DropZone from 'react-dropzone'
+import { Icon } from 'antd';
+import React, { useState } from 'react';
+import DropZone from 'react-dropzone';
+import Axios from 'axios';
+//import { response } from 'express';
 
-function FileUpload() {
+function FileUpload(props) {
+
+  const [Images, setImages] = useState([])
+
+  const onDrop =(files) =>{
+
+    let formData = new FormData();
+    const config = {
+      header: {'content-type': 'multipart/form-data'}
+    }
+    formData.append("file", files[0])
+
+
+    //Save the Image in node server
+    Axios.post('/api/product/uploadImage', formData ,config)
+    .then(response => {
+
+      if(response.data.success){
+        setImages([...Images, response.data.image])
+        props.refreshFunction([...Images, response.data.image])
+      } else {
+        alert('Failed to save the Image to Server')
+      }
+    })
+  }
+  
+  const onDelete = (image) => {
+    const currentIndex = Images.indexOf(image)
+
+    let newImages = [...Images]
+    newImages.splice(currentIndex, 1)
+
+    setImages(newImages)
+    props.refreshFunction(newImages)
+  }
+
   return (
     <div style={{ display:'flex', justifyContent:'space-between'}}>
       <DropZone
-      onDrop
-      multiple
-      maxSize
+      onDrop={onDrop}
+      multiple={false}
+      maxSize={800000000}
       >
-
-        
-
+        {({getRootProps, getInputProps}) => (
+          <div style={{ width: '300px', height:'240px', border:'1px solid lightgray',
+          display:'flex', alignItems:'center', justifyContent:'center'}}
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <Icon type="plus" style={{ fontSize: '3rem'}} />
+          </div>
+        )}
       </DropZone>
+      <div style={{ display:'flex', width:'350px', height:'240px', overflowX:'scroll' }}>
+        {Images.map((image, index) => (
+          <div onClick={() => onDelete(image)}>
+            <img style={{ minWidth: '300px', width:'300px', height:'240px'}} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
+          </div>
+        ))}          
+      </div>
     </div>
   )
 }
